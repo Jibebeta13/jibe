f828dbb535c19ced3f2535c834d6e396f9dda219
Alex Stomp <alex.stomp@8to18.com>
2013-05-31 11:35:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jibe::Application.config.secret_token = '434ade6a22cf73d7c6948cc9e9bf0400df2e3f95d43c2fb1b08cc0b2807abf225a6efee9b84edd14dc3f9482b9be046960f276d1ba3356a5816cf5d22c6841ed'
